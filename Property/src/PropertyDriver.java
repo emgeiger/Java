d0dd8bd16b77690e5c912146a470c7882e70fa84
emgeiger <36167405+emgeiger@users.noreply.github.com>
2023-09-29 17:57:05 -0500

    Creating the Java Property project
    Creating the Property Java project.

---
+++ Property/src/PropertyDriver.java
@@ -0,0 +1,32 @@
+public class PropertyDriver
+
+// This is the driver class for the Property class.
+// It creates a Property object.
+
+// Author: Matthew Holmes
+
+// Date: 12/1/18
+
+// Class: CSCI 1933
+
+// Instructor: Dr. Dana
+
+// Assignment: PropertyDriver
+
+// Description: This is the driver class for the Property class.
+// It creates a Property object.
+
+// Input: None
+
+// Output: A Property object.
+
+// Variables: None
+
+// Methods: None
+
+{
+    public static void main(String[] args)
+    {
+        Property property = new Property();
+    }
+}
